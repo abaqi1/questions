b308d57413829051e847d4637835ed815067a28e
Araiz Baqi <ariknowsalgos@gmail.com>
2024-12-04 23:05:59 -0600

    Garbage collection and secret removal

--- FirebaseConfig.ts
+++ FirebaseConfig.ts
@@ -5,24 +5,14 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-
-// import { 
-//     FIREBASE_API_KEY,
-//     FIREBASE_AUTH_DOMAIN,
-//     FIREBASE_PROJECT_ID,
-//     FIREBASE_STORAGE_BUCKET,
-//     FIREBASE_MESSAGING_SENDER_ID,
-//     FIREBASE_APP_ID
-// } from '@env';
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBlwjzm7FSKp-kg7khiEMP4H04KIImlLbI",
-    authDomain: "ariapp-9ec5b.firebaseapp.com",
-    projectId: "ariapp-9ec5b",
-    storageBucket: "ariapp-9ec5b.firebasestorage.app",
-    messagingSenderId: "1084398831066",
-    appId: "1:1084398831066:web:03bb81f427cfbd3c642ce3"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
 };
 
 // Initialize Firebase
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,7 @@
 import { Stack } from "expo-router";
-import CreateGroup from './CreateGroup';
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen
-        name="CreateGroup"
-        options={{
-          title: "Create Group"
-        }}
-      />
-    </Stack>
+    <Stack />
   );
 }
